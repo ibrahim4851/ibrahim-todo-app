ef9c27853596f9281ca0b4478d1a049d69503490
ibrahim4851 <erogluih2001@gmail.com>
2021-08-05 11:45:29 +0300

    db3

--- src/main/java/com/felece/todoapp/config/SecurityConfig.java
+++ src/main/java/com/felece/todoapp/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-@Configuration
+@Configuration//
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
